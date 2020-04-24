6629c38feecfc7423a55ecca102785fcb0964885
archliner <33724068+archliner@users.noreply.github.com>
2020-04-23 21:04:59 -0700

    style

--- src/containers/profile.container.js
+++ src/containers/profile.container.js
@@ -2,7 +2,15 @@ import React from "react";
 import {connect} from 'react-redux';
 import {withRouter} from "react-router";
 import {checkLoggedIn, getUserByUsername, fetchFollowingPlaylists, updateProfile} from '../actions/user.action'
-import {Button} from "reactstrap";
+import Card from 'react-bootstrap/Card';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 class Profile extends React.Component {
     constructor() {
@@ -32,6 +40,23 @@ class Profile extends React.Component {
 
         return (
             <div>
+
+                {/* <Form className="justify-content-md-center">
+                    <Form.Row className="justify-content-md-center">
+                        <Form.Group as={Col} controlId="formGridPassword" xs lg="4">
+                            <Form.Label></Form.Label>
+                            <Form.Control as="textarea" placeholder="New Bio here..." rows="3" onChange={(e) => this.handleChange(e, 'bio')} />
+                        </Form.Group>
+                    </Form.Row>
+                    <br/>
+                    <Form.Group as={Row} className="justify-content-md-center">
+                        <Col xs lg="4" >
+                            <Button onClick={() => this._handleUpdateProfile()} block variant="primary" type="button" disabled={this.props.inFlight}>
+                                Edit
+                            </Button>
+                        </Col>
+                    </Form.Group>
+                </Form> */}
                 <form>
                     {/* {error} */}
                     <label> New Bio:
@@ -39,11 +64,6 @@ class Profile extends React.Component {
                             disabled={this.props.inFlight}
                             value={this.state.bio}
                             onChange={(e) => this.handleChange(e, 'bio')}/> </label>
-                    {/* <label> New Password:
-                        <input type="text"
-                            disabled={this.props.inFlight}
-                            value={this.state.password}
-                            onChange={(e) => this.handleChange(e, 'password')}/> </label> */}
                     <input type="button" value="Update" onClick={() => this._handleUpdateProfile()} disabled={this.props.inFlight}/>
                 </form>
             </div>
@@ -57,7 +77,7 @@ class Profile extends React.Component {
         let playlistHeader = ""
         if (followingPlaylists) {
             followingPlaylists = this.props.playlists.map((item, i) => {
-                return <li key={i}>{item}</li>
+                return <ListGroupItem key={i}>{item}</ListGroupItem>
             });
             playlistHeader = "Following playlists"
         }
@@ -68,20 +88,36 @@ class Profile extends React.Component {
         }
         else if (status === "LOGGEDIN") {
             profile = <div>
-                <h3>Username: {user.username}</h3>
-                {/* <h3>Password: {user.password}</h3> */}
+                <Card style={{ width: '25rem'}}>
+                <Card.Img variant="top" src="https://cdn2.iconfinder.com/data/icons/social-media-2173/95/Network_Social_Instagram_Internet_Twitter_Facebook_30-512.png" />
+                <Card.Body>
+                    <Card.Title>{user.username}</Card.Title>
+                    <Card.Text>
+                    <b>Bio Info:</b> {user.bio || "This guy is lazy, nothing was provided here."}
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem><b>Following Playlists</b></ListGroupItem>
+                    {followingPlaylists}
+                </ListGroup>
+                </Card>
+                {/* <h3>Username: {user.username}</h3>
                 <h3>Bio Info: {user.bio || "This guy is lazy, nothing was provided here."}</h3>
                 <h5>{playlistHeader}</h5>
                 <ul>{followingPlaylists}</ul>
-                <br />
-                <h3>Update your profile here</h3>
+                <br /> */}
             </div>
         }
+
         return (
             <div>
-                <h1>Profile Page</h1>
-                {profile}
-                {this._updateProfile()}
+                <Jumbotron>
+                    <Container>
+                        <h1 className="text-center">My Profile</h1>
+                        {this._updateProfile()}
+                        {profile}
+                    </Container>
+                </Jumbotron>
             </div>
         );
     }
--- src/containers/youtube.container.js
+++ src/containers/youtube.container.js
@@ -6,6 +6,10 @@ import {Redirect} from "react-router";
 import {Link} from 'react-router-dom';
 import {checkLoggedIn, getUserByUsername} from '../actions/user.action';
 import {fetchPosts, postDetail} from '../actions/youtube.action';
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 
 class Youtube extends React.Component {
     constructor() {
@@ -34,21 +38,20 @@ class Youtube extends React.Component {
             </tr>));
         return (
             <div>
-                <h2>Welcome To Post Ground!</h2>
-                <table>
+                <Table striped bordered hover responsive>
                     <thead>
                     <tr>
-                        <th>PlayList</th>
-                        <th>ListName</th>
-                        <th>Poster</th>
-                        <th>Date</th>
-                        {/* <th></th> */}
+                        <th style={{width: "30%"}}>Playlist</th>
+                        <th style={{width: "15%"}}>ListName</th>
+                        <th style={{width: "15%"}}>Poster</th>
+                        <th style={{width: "25%"}}>Date</th>
+                        <th>Go to detail</th>
                     </tr>
                     </thead>
                     <tbody>
                         {postRows}
                     </tbody>
-                </table>
+                </Table>
             </div>
         )
     }
@@ -62,16 +65,22 @@ class Youtube extends React.Component {
         const status = this.props.routeState.state;
         var addButton = '';
         if (status === "LOGGEDIN") {
-            addButton = <a type='button' href="/youtube/addpost">Add Post</a>
+            // addButton = <a type='button' href="/youtube/addpost">Add Post</a>
+            addButton = <Button block variant="primary" href="/youtube/addpost">New Post</Button>
         } else if (status === "LOGGEDOUT") {
             addButton = <h4>Plaease login to add a post</h4>
         }
         return (
             <div>
-                <h1>Youtube Page</h1>
-                {addButton}
-                {this._renderPostList()}
-
+                <Jumbotron>
+                    <Container>
+                        <h1 className="text-center">Explore Youtube Posts</h1>
+                        <br/>
+                        <h4>Here are the posts</h4>
+                        {addButton}
+                        {this._renderPostList()}
+                    </Container>
+                </Jumbotron>
             </div>
         );
     }
